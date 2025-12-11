13f0a8946ef500cbb13b1cb03dbfcfa7d93cf981
parmjeet <iamparmjeetmishra@gmail.com>
2025-12-11 23:48:59 +0530

    Update=>Motion basic

--- src/app/(animation)/11motion/page.tsx
+++ src/app/(animation)/11motion/page.tsx
@@ -1,3 +1,34 @@
+"use client";
+import { motion } from "motion/react";
+
 export default function Page() {
-  return <h1 className="text-2xl">Hello Motion</h1>;
+  return (
+    <main className="bg-black text-white h-screen flex flex-col gap-10 items-center justify-center">
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: -40,
+        }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, ease: "easeIn" }}
+      >
+        <h1 className="text-4xl">Hello Motion</h1>
+      </motion.div>
+      <motion.button
+        className="bg-gray-700 text-4xl text-gray-100 p-2"
+        whileHover={{
+          scale: 1.05,
+          y: -2,
+        }}
+        whileTap={{
+          scale: 0.9,
+          y: 1,
+        }}
+        transition={{ type: "spring", stiffness: 400, damping: 4 }}
+      >
+        Motion Button
+      </motion.button>
+    </main>
+  );
 }
+
